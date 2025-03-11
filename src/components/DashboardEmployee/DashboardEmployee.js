8a6e60240f1e0b54afbdb599e3ffae6c69a1d2b2
teepakornLT <teeprakorn.toey@gmail.com>
2025-03-11 09:19:49 +0700

    VERSION 1.0

--- src/components/DashboardEmployee/DashboardEmployee.js
+++ src/components/DashboardEmployee/DashboardEmployee.js
@@ -3,12 +3,14 @@ import Navbar from '../NavigationBar/NavigationBar';
 import styles from './DashboardEmployee.module.css';
 
 function DashboardEmployee() {
+
   return (
     <div className={styles.container}>
       <Navbar />
       <div className={styles.mainContent}>
         <h1 className={styles.heading}>DashBoard</h1>
-      </div>
+        <iframe title="ForeCast_pm25_1" width="1220" height="2760" src="https://app.powerbi.com/view?r=eyJrIjoiOTVkZjYzMTctOThjOS00ZjIxLThkNmYtNDI1ZTdmOTEzN2JkIiwidCI6IjU3ZDY5NWQ0LWFkODYtNDRkMy05Yzk1LTcxNzZkZWFjZjAzZCIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
     </div>
   );
 }
